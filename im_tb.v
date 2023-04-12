0a2d5ac0d7cd1ce916d6b23f888d4c5f4de448c2
chandyego <chandlerjuego101@gmail.com>
2023-04-11 20:48:44 -0700

    opcode testing. instruction is one step ahead when cycling for some rzn:

--- im_tb.v
+++ im_tb.v
@@ -12,7 +12,7 @@ initial begin
     $display("im testing.");
     taylor.pc = 0;
     taylor.inst = 0;
-    clk = 1;
+    clk = 0;
 end
 
 // clk generator
@@ -22,50 +22,8 @@ always #((CLK_PERIOD)/2) clk <= ~clk; // 10ns
 integer counter = 0;
 
 always @(posedge clk) begin
-    if (counter < 14) begin
-        if (counter == 0) begin
-            $display("[C], PC: %0d, Instruction: %8h", $time, taylor.pc, taylor.inst);
-        end
-        if (counter == 1) begin
-            $display("[O], PC: %0d, Instruction: %8h", $time, taylor.pc, taylor.inst);
-        end
-                if (counter == 2) begin
-            $display("[R], PC: %0d, Instruction: %8h", $time, taylor.pc, taylor.inst);
-        end
-        if (counter == 3) begin
-            $display("[N], PC: %0d, Instruction: %8h", $time, taylor.pc, taylor.inst);
-        end
-        if (counter == 4) begin
-            $display("[E], PC: %0d, Instruction: %8h", $time, taylor.pc, taylor.inst);
-        end
-        if (counter == 5) begin
-            $display("[L], PC: %0d, Instruction: %8h", $time, taylor.pc, taylor.inst);
-        end
-        if (counter == 6) begin
-            $display("[I], PC: %0d, Instruction: %8h", $time, taylor.pc, taylor.inst);
-        end
-        if (counter == 7) begin
-            $display("[A], PC: %0d, Instruction: %8h", $time, taylor.pc, taylor.inst);
-        end
-        if (counter == 8) begin
-            $display("[S], PC: %0d, Instruction: %8h", $time, taylor.pc, taylor.inst);
-        end
-        if (counter == 9) begin
-            $display("[T], PC: %0d, Instruction: %8h", $time, taylor.pc, taylor.inst);
-        end
-        if (counter == 10) begin
-            $display("[R], PC: %0d, Instruction: %8h", $time, taylor.pc, taylor.inst);
-        end
-        if (counter == 11) begin
-            $display("[E], PC: %0d, Instruction: %8h", $time, taylor.pc, taylor.inst);
-        end
-        if (counter == 12) begin
-            $display("[E], PC: %0d, Instruction: %8h", $time, taylor.pc, taylor.inst);
-        end
-        if (counter == 13) begin
-            $display("[T], PC: %0d, Instruction: %8h", $time, taylor.pc, taylor.inst);
-        end
-
+    if (counter < 9) begin
+        //$display("[%0d], PC: %0d, Instruction: %8h", $time, taylor.pc, taylor.inst);
         taylor.pc <= taylor.pc + 1;
         counter <= counter + 1;
     end
@@ -81,47 +39,26 @@ taylor taylor(
 );
 
 initial begin
-    // Load instruction 1: ADDI $t0, $zero, 0x1234
-    taylor.rom[0] = 32'h20081234;
-    // Load instruction 2: ADDI $t1, $zero, 0xABCD
-    taylor.rom[1] = 32'h2009ABCD;
-    // Load instruction 3: ADD $s0, $t0, $t1
-    taylor.rom[2] = 32'h01298820;
-    // Load instruction 4: SUB $s1, $t0, $t1
-    taylor.rom[3] = 32'h01298822;
-    // Load instruction 5: AND $s2, $t0, $t1
-    taylor.rom[4] = 32'h01298822;
-    // Load instruction 6: OR $s3, $t0, $t1
-    taylor.rom[5] = 32'h01298825;
-    // Load instruction 7: SLT $s4, $t0, $t1
-    taylor.rom[6] = 32'h0129882A;
-    // Load instruction 8: ADDI $t2, $zero, 0x5678
-    taylor.rom[7] = 32'h200A5678;
-    // Load instruction 9: ADD $s5, $t1, $t2
-    taylor.rom[8] = 32'h012A8820;
-    // Load instruction 10: SUB $s6, $t1, $t2
-    taylor.rom[9] = 32'h012A8822;
-    // Load instruction 11: AND $s7, $t1, $t2
-    taylor.rom[10] = 32'h012A8824;
-    // Load instruction 12: OR $t3, $t1, $t2
-    taylor.rom[11] = 32'h012A8825;
-    // Load instruction 13: SLT $t4, $t1, $t2
-    taylor.rom[12] = 32'h012A882A;
-    // Load instruction 14: NOP
-    taylor.rom[13] = 32'h00000000;
-
-     // Reset romory and start simulation
+    // ADDI
+    taylor.rom[0] = 32'h20100005;
+    // ADDI
+    taylor.rom[1] = 32'h20100005;
+    // ADD
+    taylor.rom[2] = 32'h012A4820;
+    // SUB
+    taylor.rom[3] = 32'h012A4822;
+    // SUB
+    taylor.rom[4] = 32'h012A4822;
+    // BEQ
+    taylor.rom[5] = 32'h112A002A;
+    // BEQ
+    taylor.rom[6] = 32'h112A002A;
+    // LOAD WORD
+    taylor.rom[7] = 32'h8C0A0000;
+    // ORI
+    taylor.rom[8] = 32'h34E700FF;
+    
     #2;
-    /**
-    rom <= 0;
-    clk <= 0;
-    #2;
-    clk <= 1;
-    #2;
-    clk <= 0;
-    #2;
-    clk <= 1;
-    **/
 end
 
 endmodule
--- taylor.v
+++ taylor.v
@@ -17,12 +17,68 @@ end
 reg [31:0] rom [0:1023]; // instruction memory
 reg [31:0] inst; // current instruction
 
+/**Instruction types**/
+reg [31:26] opcode;
+reg [25:21] rs;
+reg [20:16] rt;
+reg [15:11] rd;
+reg [10:6] shamt;
+reg [5:0] funct;
+
+reg [15:0] i_imm;
+
+reg [25:0] j_addr;
+
+
 /**Instruction Fetch**/
 always @ (posedge(clk)) begin
+    /**Instruction Fetch**/
     inst <= rom[pc];
+
+    /**Instruction Decode, Register Fetch**/
+    /*
+    R-type: opcode [31:26], rs[25:21], rt[20:16], rd[15:11], shamt[10:6], funct[5:0]
+    I-type: opcode [31:26], rs[25:21], rt[20:16], imm[15:0]
+    J-type: opcode [31:26], addr[25:0]
+    */
+    opcode <= inst[31:26];
+
+    case (opcode)
+    // R-type
+    6'b0: begin
+        $display("R-Type Instruction: %h\n", inst);
+    end
+    // addi
+    6'h8: begin 
+        $display("Addi Instruction: %h\n", inst);
+    end
+    // addiu
+    6'h9: begin
+        $display("Addiu Instruction: %h\n", inst);
+    end
+    // andi
+    6'hc: begin
+        $display("Andi Instruction: %\n", inst);
+    end
+    // beq
+    6'h4: begin
+        $display("Beq Instruction: %h\n", inst);
+    end
+    // bne
+    6'h5: begin
+        $display("Addiu Instruction: %h\n", inst);
+    end
+    // lw
+    6'h23: begin
+        $display("Lw Instruction: %h\n", inst);
+    end
+    // ori
+    6'hd: begin
+        $display("Ori Instruction: %h\n", inst);
+    end
+
+    endcase
+
 end
-
-/**Instruction Decode, Register Fetch**/
-
 
 endmodule
