c138e6e3ff635ccde9ff72512bbbc5d1d9e0ec5e
chandyego <chandlerjuego101@gmail.com>
2023-04-11 17:19:57 -0700

    instruction fetch testing

--- taylor.v
+++ taylor.v
@@ -5,7 +5,24 @@
 module taylor(
     input clk,
     input rst,
-    output reg [31:0] newPC
+    output reg [9:0] pc // 1000 instructions
 );
+
+always @(posedge clk) begin
+    if (rst) begin
+        pc <= 10'b0;
+    end
+end
+
+reg [31:0] rom [0:1023]; // instruction memory
+reg [31:0] inst; // current instruction
+
+/**Instruction Fetch**/
+always @ (posedge(clk)) begin
+    inst <= rom[pc];
+end
+
+/**Instruction Decode, Register Fetch**/
+
 
 endmodule
